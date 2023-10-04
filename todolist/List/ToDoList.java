207140f46e5cbf3b92027151ba7f8c926e0519a0
Hassan Bazzi <133389011+hassanbazzi1@users.noreply.github.com>
2023-10-04 20:16:48 +0300

    Update ToDoList.java

--- todolist/List/ToDoList.java
+++ todolist/List/ToDoList.java
@@ -6,16 +6,13 @@ import java.util.List;
 public class ToDoList {
     private List<Task> tasks;
     private static final String TASKS_FILE = "tasks.ser"; // File to store tasks
-
     public ToDoList() {
         loadTasksFromFile();
     }
-
     public void addTask(Task task) {
         tasks.add(task);
         saveTasksToFile();
     }
-
     public void markTaskAsComplete(int index) {
         if (isValidIndex(index)) {
             Task task = tasks.get(index);
@@ -25,7 +22,6 @@ public class ToDoList {
             System.out.println("Invalid task index.");
         }
     }
-
     public void viewTasks() {
         if (tasks.isEmpty()) {
             System.out.println("No tasks to display.");
@@ -38,7 +34,6 @@ public class ToDoList {
             }
         }
     }
-
     public void removeTask(int index) {
         if (isValidIndex(index)) {
             tasks.remove(index);
@@ -48,26 +43,23 @@ public class ToDoList {
             System.out.println("Invalid task index.");
         }
     }
-
     private boolean isValidIndex(int index) {
         return index >= 0 && index < tasks.size();
     }
-
     private void loadTasksFromFile() {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(TASKS_FILE))) {
             tasks = (List<Task>) in.readObject();
         } catch (IOException | ClassNotFoundException e) {
-            // If the file doesn't exist or reading fails, initialize tasks as an empty list.
+            // If the file doesn't exist 
             tasks = new ArrayList<>();
         }
     }
-
     private void saveTasksToFile() {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(TASKS_FILE))) {
             out.writeObject(tasks);
         } catch (IOException e) {
             e.printStackTrace();
-            // Handle exceptions, e.g., if saving fails
+            // Handle exceptions
         }
     }
 }
